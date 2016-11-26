25ce816d3912dbabc98e6946ff05c68ad988500b
Sal Tardibuono <s.tardibuono@live.com>
2016-11-25 16:50:36 -0800

    Finished Static Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get 'static_pages/contact'
+
   get 'static_pages/about'
 
   get 'static_pages/home'
@@ -7,5 +10,5 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'application#hello'
+  root 'static_pages#home'
 end
