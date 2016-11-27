443165f912a3166f3a48359c0dfa4715a2246e04
Sal Tardibuono <s.tardibuono@live.com>
2016-11-26 20:09:16 -0800

    Finished layout and routes, added User controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-
-  get 'static_pages/contact'
-
-  get 'static_pages/about'
-
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root 'static_pages#home'
+  get '/contact', to: 'static_pages#contact'
+  get '/about', to: 'static_pages#about'
+  get '/help', to: 'static_pages#help'
+  get '/signup', to: 'users#new', as: 'signup'
 end
